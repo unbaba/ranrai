fae9c41a3f1aac094d306f3ab908a5c497c76b90
yteshima <vc11807@unbaba.(none)>
2013-04-30 18:30:07 +0900

    added MLDISP_RefreshAndPrint()

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 #define cons_MSG(...) printf(__VA_ARGS__)
 
@@ -283,21 +284,88 @@ cons_MSG("===================================\n");
 	return iResult;
 }
 
+/*
+	引数で渡された文字列が何行になるかを返します
+	渡す文字列は暫定的に 10000文字までとします
+	
+	必須:
+		文字列の終わりに '\0' があること
+		文字列の終わりに '\0' が無い場合の動作は未定義です(大抵 Segmentation Fault)
+
+	戻り値:
+		行数(n行)
+*/
+#define MLSTR_GET_HOW_MANY_LINES_MAX_LEN 10000
+
+extern int MLSTR_GetHowManyLines
+(const char *a_pStr,	// 文字列
+ const int   a_iStrLen	// 文字列長
+)
+{
+	int i = 0;
+	int iLineCnt = 0;
+	const char *pStr = a_pStr;
+
+	for(i=0; i<MLSTR_GET_HOW_MANY_LINES_MAX_LEN; i++)
+	{
+		if( '\0' != *(pStr + i) )
+		{
+			if ( '\n' != *(pStr + i) )
+			{
+				// check next char.
+			}
+			else
+			{
+				iLineCnt++;
+			}
+		}
+		else
+		{
+			break; // Str End.
+		}
+	}
+
+	return iLineCnt;
+}
+
+/*
+	引数で受け取った文字列を画面更新しながら表示します
+	
+	戻り値:
+		ML_TRUE  正常終了
+		ML_FALSE 異常終了 // 今のところエラー無し
+*/
+extern int MLDISP_RefreshAndPrint
+(const char *a_pStr,	// 文字列
+ const int   a_iStrLen	// 文字列長
+)
+{
+	ML_BOOL bRet = ML_FALSE;
+
+	if( NULL != a_pStr )
+	{
+		// 画面のクリア
+		printf("\x1b[2J"); // 画面のクリア
+		printf("\x1b[0;0H"); // (0, 0) へカーソルを移動
+
+		// 目的の文字列を表示
+		printf(a_pStr);
+
+		bRet = ML_TRUE;
+	}
+	else
+	{
+cons_MSG("", "a_pStr is NULL!!");
+	}
+
+	return bRet;
+}
+
 int main(const char *a_pArg[])
 {
 	int iRet = ERROR_NONE;
 
 	iRet = ranrai_main();
-
-printf("\n");
-printf("1\n");
-printf("2\n");
-printf("3\n");
-printf("unbaba--\n");
-/* エスケープシーケンスをつかってカーソルを上に持っていく */
-printf("\x1b[2A");
-
-printf("unbaba--old\n");
 
 	return iRet;
 }
